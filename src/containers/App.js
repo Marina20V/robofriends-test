6911e8504688769805f0e70d0649e393fde3a906
Marina20V <meri20vit@gmail.com>
2020-09-22 13:15:10 +0300

    Add CopponentDidCatch method

--- src/containers/App.js
+++ src/containers/App.js
@@ -38,9 +38,9 @@ class App extends Component {
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                    <ErrorBoundry></ErrorBoundry>
+                    <ErrorBoundry>
                     <CardList robots={filteredRobots} />
-                    <ErrorBoundry />
+                    </ErrorBoundry>
                     </Scroll>
                 </div>
             );
